25247d426c266685dd749ba357b00aa7765d0829
naliali <nalialiangela365@gmail.com>
2024-07-10 17:40:35 +0300

    app is working

--- server/app.py
+++ server/app.py
@@ -17,37 +17,43 @@ db.init_app(app)
 api = Api(app)
 
 
-class Plants(Resource):
-
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
 
+        if plant is None:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
 
+        response_dict = plant.to_dict()
+        return make_response(jsonify(response_dict), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant is None:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        for attr in request.json:
+            setattr(plant, attr, request.json[attr])
+
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+        return make_response(jsonify(response_dict), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant is None:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
+    
+    
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
